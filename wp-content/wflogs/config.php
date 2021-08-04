3b2f8c1c517b96f2d05c0fac343e6e2d868990a4
root <root@debian.rosa>
2021-08-04 13:15:49 +0200

    Plugins de accesibilidade, cookies traducido, captcha para publicar a experiencia e seguridade

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1628676646;s:7:"authKey";s:64:"m$S?{zDASVSmD&}>IjQ-Vt)@O1fy SQyFGM2R]8)X,3DZz;A76SvHPgjqtml2[vh";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3232;}
