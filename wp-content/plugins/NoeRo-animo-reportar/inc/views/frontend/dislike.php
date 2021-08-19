c75298649762ba1c9768967ede422240a279f914
A20NoeliaAM <a20noeliaam@iessanclemente.net>
2021-08-19 21:08:07 +0200

    uso de la función mail() para mandar un email al reportar

--- wp-content/plugins/NoeRo-animo-reportar/inc/views/frontend/dislike.php
+++ wp-content/plugins/NoeRo-animo-reportar/inc/views/frontend/dislike.php
@@ -48,7 +48,19 @@
         ?>
     
 	<!-- Comento a seguinte liña para qe non aparezca o numero de xente que lle dou a like
-	Ata onde entendo eu, a variable $dislike_count sefue sumandose. Polo que se pode poñer o contador de que no 10 reporte ao administrador--»
+	Ata onde entendo eu, a variable $dislike_count segue sumandose. Polo que se pode poñer o contador de que no 10 reporte ao administrador--»-->
+
+		<?php 
+			if ($dislike_count == 2){
+				$cuerpo ="El comentario "+$comment_id+" ha sido reportado un mínimo de "+$dislike_count+" veces:\r\n ";
+			
+				mail("noelia.acuna@macrotest.es, rosa.castineiras@macrotest.es","Comentario reportardo en Eu tamén!",$cuerpo); 
+			}
+
+		?>
+
+
+
    <!-- <span class="cld-dislike-count-wrap cld-count-wrap"><?php echo $dislike_count; ?></span>-->
 		<!--Neste caso tense que ver tanto a palabra ánimo como o contador-->
 		<?php echo "Reportar"; ?>
