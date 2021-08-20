f09ab91823fe2b469e3b9babfcb47c9190280a76
root <root@debian.rosa>
2021-08-20 13:13:45 +0200

    Base de datos

--- wp-content/plugins/mailpoet/vendor-prefixed/autoload.php
+++
@@ -1,2 +0,0 @@
-<?php
- namespace MailPoetVendor; if (!defined('ABSPATH')) exit; require_once __DIR__ . '/composer/autoload_real.php'; return ComposerAutoloaderInit120d3c47c3814619bf4b5c6fface5cce::getLoader(); 
