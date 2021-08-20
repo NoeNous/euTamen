72398284cf9acdbd046a8a226afc67605eb63145
root <root@debian.rosa>
2021-08-20 10:36:03 +0200

    Proba de .xml

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1630049714;s:7:"authKey";s:64:"eB%Q9Y5).UlMLjF/~4xi({)g$[b{OEPN_a$Vb&-7WFci(A$AK9Ew[7u .,>q`rs$";s:7:"version";s:5:"1.0.4";}
